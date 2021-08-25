406f6380237fa9624a8febe706756a9cca0ce9a2
alperalt1 <alperalt@espol.edu.ec>
2021-08-24 20:03:33 -0500

    Ya se agrego los componentes de login en el primer grafo de scene

--- src/main/java/ec/edu/espol/controller/LoginController.java
+++ src/main/java/ec/edu/espol/controller/LoginController.java
@@ -7,7 +7,10 @@ package ec.edu.espol.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 /**
  * FXML Controller class
@@ -15,6 +18,13 @@ import javafx.fxml.Initializable;
  * @author ZavalaAvila
  */
 public class LoginController implements Initializable {
+
+    @FXML
+    private GridPane griplogin;
+    @FXML
+    private TextField logincorreo;
+    @FXML
+    private TextField logincontras;
 
     /**
      * Initializes the controller class.
