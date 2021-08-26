a8e912ac1b899bd2d683563b28c11eaf8c6ff344
alperalt1 <alperalt@espol.edu.ec>
2021-08-26 02:22:36 -0500

    grafo de escena pantalla de ingresar usuario o registrarse

--- src/main/java/ec/edu/espol/controller/LoginController.java
+++ src/main/java/ec/edu/espol/controller/LoginController.java
@@ -5,10 +5,15 @@
  */
 package ec.edu.espol.controller;
 
+import ec.edu.espol.compraventavehiculog6.App;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -35,11 +40,29 @@ public class LoginController implements Initializable {
     }    
 
     @FXML
-    private void credenciales(MouseEvent event) {
+    private void sceneiniciosesion(MouseEvent event) {
+        try {
+            FXMLLoader fxmloader = App.loadFXMLLoader("IngresarUsuario");
+            App.setRoot(fxmloader); 
+            IngresarUsuarioController hc= fxmloader.getController(); //recupero el controlador
+               
+            } catch (IOException ex) {
+                Alert a = new Alert(AlertType.ERROR,"No se pudo abrir el archivo");
+                a.show();
+            }
     }
 
     @FXML
-    private void ingresardatos(MouseEvent event) {
+    private void sceneregistrarse(MouseEvent event) {
+        try {
+            
+            FXMLLoader fxmloader = App.loadFXMLLoader("RegistroUsuario");
+            App.setRoot(fxmloader); 
+            RegistroUsuarioController hc= fxmloader.getController(); //recupero el controlador
+            } catch (IOException ex) {
+                Alert a = new Alert(AlertType.ERROR,"No se pudo abrir el archivo");
+                a.show();
+            }
     }
     
 }
