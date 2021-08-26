69c4f3efef2437519f79f569498f80b282400474
alperalt1 <alperalt@espol.edu.ec>
2021-08-26 02:22:13 -0500

    grafo de escena pantalla de ingresar usuario

---
+++ src/main/java/ec/edu/espol/controller/IngresarUsuarioController.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.controller;
+
+import ec.edu.espol.compraventavehiculog6.App;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author ADMIN
+ */
+public class IngresarUsuarioController implements Initializable {
+
+    @FXML
+    private TextField textcorreo;
+    @FXML
+    private TextField textcontrase;
+    @FXML
+    private Button btningresar;
+    @FXML
+    private Button btnregresar;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void Menuusuario(MouseEvent event) {
+    }
+
+    @FXML
+    private void sceneregresoinciosesion(MouseEvent event) {
+       try {
+            FXMLLoader fxmloader = App.loadFXMLLoader("Login");
+            App.setRoot(fxmloader); 
+            LoginController hc= fxmloader.getController(); //recupero el controlador
+               
+            } catch (IOException ex) {
+                Alert a = new Alert(AlertType.ERROR,"No se pudo abrir el archivo");
+                a.show();
+            }
+        
+    }
+    
+}
