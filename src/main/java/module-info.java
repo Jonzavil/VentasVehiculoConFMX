3271e2e63ab5dd72e66806f2b4d63f2e420be902
jonzavil <jonzavil@espol.edu.ec>
2021-08-30 22:51:40 -0500

    Errores solucion

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,12 @@ module ec.edu.espol.compraventavehiculog6 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires javafx.graphicsEmpty;
 
     opens ec.edu.espol.compraventavehiculog6 to javafx.fxml;
     opens ec.edu.espol.controller to javafx.fxml;
+    opens ec.edu.espol.util to javafx.fxml;
     exports ec.edu.espol.compraventavehiculog6;
     exports ec.edu.espol.controller;
-    requires java.mail;
+    exports ec.edu.espol.util; 
 }
