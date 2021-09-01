99ea16c7a7ebc9b3a450f41812fd69cdfb9c66e0
alperalt1 <alperalt@espol.edu.ec>
2021-08-31 23:17:17 -0500

    exception

---
+++ src/main/java/ec/edu/espol/model/ErrorException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.model;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ErrorException extends Exception{
+    public ErrorException(String x){
+        super(x);
+    }
+    
+}
