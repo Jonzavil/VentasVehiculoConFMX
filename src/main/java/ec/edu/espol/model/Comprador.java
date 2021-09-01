d837dd1544abb12d9fc6c89b1b4674868d40c7f6
Johnny D. Parrales <alphaware576@users.noreply.github.com>
2021-08-31 20:28:23 -0500

    changes temp

--- src/main/java/ec/edu/espol/model/Comprador.java
+++ src/main/java/ec/edu/espol/model/Comprador.java
@@ -19,33 +19,10 @@ public class Comprador extends Persona {
     public Comprador(int id, String nombre, String apellidos, String organizacion, String correoElectronico, String clave) {
         super(id,nombre,apellidos,organizacion,correoElectronico,clave);
     }
-    public static ArrayList<Comprador> readFile(String nomFile){
-        ArrayList<Comprador> compradores = new ArrayList<>();
-        try(Scanner sc = new Scanner(new File(nomFile))){
-            while(sc.hasNextLine())
-            {
-                String linea = sc.nextLine();
-                String[] tokens = linea.split("\\|");
-                Comprador c = new Comprador(Integer.parseInt(tokens[0]),tokens[1],tokens[2],tokens[3],tokens[4],tokens[5]);
-                compradores.add(c);
-            }
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        return compradores;
-    }
-    public static String searchByCorreo(ArrayList<Comprador> compradores,String correo){
-        String cn=null;
-        for(Comprador c : compradores){
-            if(c.correoElectronico.equals(correo));
-            cn= c.correoElectronico;
-        }
-        return cn;
-    }
+   
      public static Comprador registroComprador(Scanner sc, String nomfile){
-         ArrayList<Comprador> compradores = Comprador.readFile(nomfile);
-         int id = Util.nextID(nomfile);
+         ArrayList<Persona> personas = Persona.readFile(nomfile);
+         int id = Util.nextID(personas);
          System.out.println("Ingrese sus nombres: ");
          String nombres = sc.next();
          System.out.println("Ingrese sus apellidos: ");
@@ -56,34 +33,17 @@ public class Comprador extends Persona {
          String correo = sc.next();
          System.out.println("Ingrese su clave: ");
          String clave = sc.next();
-         if (correo.equals(Comprador.searchByCorreo(compradores, correo))){
+         if (correo.equals(Persona.searchByCorreo(personas, correo))){
              return null;
          }
          else{
-             Comprador c1 = new Comprador(id, nombres,apellidos,organizacion,correo,clave);   
-             c1.saveFile(nomfile);
+             Comprador c1 = new Comprador(id, nombres,apellidos,organizacion,correo,clave);
+             personas.add(c1);
+             Persona.saveFile(nomfile, personas);
+             //c1.saveFile(nomfile);
              return c1;
          }
      }
-     public static boolean compararCorreoYContraseña(String nomfile,String correo,String contraseña){
-        ArrayList<Comprador> compradores = Comprador.readFile(nomfile);
-        String c=null;
-        try 
-        {
-            contraseña = Util.toHexString(Util.getSHA(contraseña));
-            contraseña = Util.toHexString(Util.getSHA(contraseña));
-        }
-        // For specifying wrong message digest algorithms 
-        catch (NoSuchAlgorithmException e) { 
-            System.out.println("Exception thrown for incorrect algorithm: " + e); 
-        }
-            for(Comprador v: compradores){
-                if(v.correoElectronico.equals(correo)){
-                    c=v.clave;
-                }
-            } 
-        return contraseña.equals(c);
-    }
 }
     
    
